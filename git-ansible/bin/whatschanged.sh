8c6b0d76cf802d57d2aac373f9b822d6fcff872c
divious1 <josehelps@gmail.com>
2015-09-17 07:20:13 +0000

    added git checkin logic to search head deployment

---
+++ git-ansible/bin/whatschanged.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+EXPECTED_ARGS=1
+E_BADARGS=65
+
+if [ $# -lt 1 ]
+then
+echo "Scripted input for splunk to monitors github changes locally"
+echo "Usage: `basename $0` <path>"
+echo "Example `basename $0`  /etc/ansible/"
+echo ""
+echo "path: of github hosted directory"
+  exit $E_BADARGS
+fi
+
+if [ $# -gt $EXPECTED_ARGS ]
+then
+echo "Too many arguments"
+        exit $E_BADARGS
+fi
+
+location=$1
+cd $location
+if [ -a $SPLUNK_HOME/.changes ]
+then
+  git whatchanged > $SPLUNK_HOME/.current_changes
+  diff $SPLUNK_HOME/.changes $SPLUNK_HOME/.current_changes -n
+  mv $SPLUNK_HOME/.current_changes $SPLUNK_HOME/.changes
+else
+  git whatchanged > $SPLUNK_HOME/.changes
+  cat $SPLUNK_HOME/.changes
+fi
+
+
